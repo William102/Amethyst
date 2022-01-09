052952daee2ba3e083a6aec384b9535f3122d3da
fatima angulo barba <fatimaa@vandenrobotics.com>
2022-01-09 11:44:08 -0800

    match scouting
    i think i got the opening page to work:)

--- app/src/main/java/org/robovikes/amethyst/Fragments/MatchScoutingFragment.java
+++ app/src/main/java/org/robovikes/amethyst/Fragments/MatchScoutingFragment.java
@@ -6,6 +6,12 @@ import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,12 +26,47 @@ import org.robovikes.amethyst.databinding.FragmentMatchScoutingBinding;
 
 public class MatchScoutingFragment extends Fragment {
 
+    private Spinner teamSpinner;
     private FragmentMatchScoutingBinding binding;
+    private int match = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentMatchScoutingBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        setupSpinners();
+
+        Button button = root.findViewById(R.id.startMatch);
+        Button plusButton = root.findViewById(R.id.button2);
+        Button minusButton = root.findViewById(R.id.button);
+        TextView matchNum = root.findViewById(R.id.matchNumber);
+        matchNum.setText(String.valueOf(match));
+        plusButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                match++;
+                matchNum.setText(String.valueOf(match));
+            }
+        });
+
+        minusButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(match > 1){
+                    match--;
+                    matchNum.setText(String.valueOf(match));
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(getActivity(),R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_scouting);
+
+            }
+        });
 
         final Handler handler = new Handler(Looper.getMainLooper());
         handler.postDelayed(new Runnable() {
@@ -44,4 +85,28 @@ public class MatchScoutingFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    public void setupSpinners(){
+        View root = binding.getRoot();
+
+        teamSpinner = root.findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> teamAdapter = ArrayAdapter.createFromResource(root.getContext(),R.array.teams,android.R.layout.simple_spinner_item);
+        teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamSpinner.setAdapter(teamAdapter);
+
+        teamSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String choice = parent.getItemAtPosition(position).toString();
+                Toast.makeText(binding.getRoot().getContext(), choice, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
 }
+
